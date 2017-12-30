0b49b23161507e8a2fa016051b664bc3fef81c24
Andraz Simcic <as7495@student.uni-lj.si>
2017-12-30 20:45:28 +0000

    Small mistake fixed

--- data/scripts/RoboSim.js
+++ data/scripts/RoboSim.js
@@ -246,7 +246,7 @@ function handleTextureLoaded(texture) {
   gl.bindTexture(gl.TEXTURE_2D, texture);
   gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
   gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
-  //gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
   gl.generateMipmap(gl.TEXTURE_2D);
 
   gl.bindTexture(gl.TEXTURE_2D, null);
